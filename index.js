acd4e5b86babe0d6cae3905d6578906d83fffbd5
phamquocviet99 <phamquocviet1211999@gmail.com>
2023-07-04 11:36:24 +0700

    q

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" })); // limit from front-end data 30MB
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/feedback", feedBackRouter);
 app.use("/demands", demandRouter);
 app.use("/myCart", myCartRouter);
